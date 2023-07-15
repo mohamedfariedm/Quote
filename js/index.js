f0c777e2a62ca8f3105c315b01bd88ac8cb7be73
mohamedfariedm <mohamed.faried23267@gmail.com>
2023-07-15 21:36:02 +0300

    sec commit

--- js/index.js
+++ js/index.js
@@ -10,13 +10,26 @@ var alltext=
 ` “If you tell the truth, you dont have to remember anything”― Mark Twain ` , 
 ` “Always forgive your enemies; nothing annoys them so much.” ― Oscar Wilde ` 
 ];
+var allHeading=
+[
+` faried `,    
+` mohamed`,
+`mahmod`,
+`ahmed` ,
+`hassan`,
+` mark roson` , 
+` tim-bearners-lee` 
+];
 var matchNum=0;
-
+var box="";
 function changetext() {
     //كده مفيش حاجه هتتكرر ورا بعض بنفس الكلمه 
     var ranNum = Math.floor( Math.random() * alltext.length );
     if(ranNum!=matchNum){
-        var box= `${alltext[ranNum]}`;
+        box= `
+        <h1 class="mt-5 fs-1 text-center fw-bolder">${allHeading[ranNum]}</h1>
+        <p class="mt-5 fs-3 text-center fw-lighter">${alltext[ranNum]}</p>
+        `;
         seentext.innerHTML=box;
         matchNum=ranNum;
         // console.log(ranNum)
